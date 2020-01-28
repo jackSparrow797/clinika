11859ae3c4ed0422ae2e131936938ff09e5e4c17
Медведев Евгений <medvedev@adv.ru>
2020-01-28 10:31:47 +0400

    init project

---
+++ public/assets/chunks/common/header.tpl
@@ -0,0 +1,65 @@
+<header class="row position-relative">
+    <div class="container d-none d-lg-block">
+        
+        <div class="row align-items-center justify-content-between">
+            
+            <div class="col-lg-auto">
+                <div class="row align-items-center justify-content-between">
+                    
+                    <div class="col-auto">
+                        [[$address]]
+                    </div>
+                </div>
+            </div>
+            
+            <!--div class="col-lg-auto menu-mobile" id="menu-mobile"-->
+            <div class="col-lg-auto menu-mobile" id="menu-mobile">
+                <div class="row justify-content-end align-items-center ">
+                    
+                    <div class="col-lg-auto">
+                        <a href="#modal-question" class="link link-grey" data-toggle="modal">Задать вопрос специалисту</a>
+                    </div>
+                    
+                    <div class="col-lg-auto">
+                        <a href="[[++license]]"  class="link link-blue"  target="_blank" >[[++license-name]]</a>
+                    </div>
+                    
+                    <div class="col-lg-auto">
+                        <p class="font-black">[[++phone]]</p>
+                    </div>
+                    <div class="col-lg-auto">
+                        <div class="row socials-links mb-lg-0 mb-3">
+                            [[$socials]]
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+    </div>
+    <div class="container-fluid menu-desktop d-none d-lg-block floating">
+        <div class="row">
+            <div class="container  ">
+                <div class="row align-items-center">
+                    <div class="col-auto d-lg-none">
+                        <a href="#menu-mobile" class="menu-mobile-toggle">
+                            <img src="assets/design/images/1VLdNQTj6h.png" alt="menu">
+                            <img class='close' src="assets/design/images/2xugxSLUuT.png" alt="close">
+                        </a>
+                    </div>
+                    <div class="col-lg-2">
+                        <a href="/">
+                            <img src="/assets/design/images/18e2dhGY6M.png" alt="logo">
+                        </a>
+                    </div>
+                    <div class="col">
+                         [[$menu]]
+                     </div>
+                    <div class="col-lg-2">
+                        <a href="#modal-order" data-toggle="modal" class="btn btn-primary br-50 btn-block">Записаться на прием</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const webpack = require('webpack');
 
 module.exports = {
     entry: [
@@ -8,19 +9,70 @@ module.exports = {
     watch: true,
     output: {
         filename: "bundle.js",
+        publicPath: '/assets/design/',
+        chunkFilename: '[name].bundle.js',
         path: path.resolve(__dirname + '/public/assets', 'design')
     },
     module: {
         rules: [
+
+            {
+                test: /\.(gif|png|jpe?g|svg)$/i,
+                use: [
+                    'file-loader?name=[sha512:hash:base64:10].[ext]&outputPath=images',
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            mozjpeg: {
+                                progressive: true,
+                                quality: 65
+                            },
+                            // optipng.enabled: false will disable optipng
+                            optipng: {
+                                enabled: false,
+                            },
+                            pngquant: {
+                                quality: [0.65, 0.90],
+                                speed: 4
+                            },
+                            gifsicle: {
+                                interlaced: false,
+                            },
+                            // the webp option will enable WEBP
+                            webp: {
+                                quality: 75
+                            }
+                        }
+                    },
+                ],
+            },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // fallback to style-loader in development
                     MiniCssExtractPlugin.loader,
                     'css-loader',
-                    'sass-loader',
+                    {
+                        loader: 'sass-loader',
+                        options: {
+                            sourceMap: true,
+                        },
+                    },
                 ],
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            }
         ]
     },
     plugins: [
@@ -29,6 +81,10 @@ module.exports = {
             // both options are optional
             filename: '[name].css',
             chunkFilename: '[id].css',
-        })
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery'
+        }),
     ],
 }
